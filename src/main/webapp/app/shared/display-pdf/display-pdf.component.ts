b0abdbd5283c44431ca1048fde6dbb1c18a95f1d
Martin Fournier <martinfournier@compica.com>
2024-12-01 20:38:45 -0500

    add component display-pdf

---
+++ src/main/webapp/app/shared/display-pdf/display-pdf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-display-pdf',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-pdf.component.html',
+  styleUrl: './display-pdf.component.scss',
+})
+export class DisplayPdfComponent {}
